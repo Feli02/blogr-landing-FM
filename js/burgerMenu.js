bb4ddc0493546e81ddc0400fe082b8cb786e307b
Feli02 <felipeberraz02@gmail.com>
2021-08-24 19:41:59 -0300

    update burger menu functionality with css adding visbility to improve efficiency

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -6,7 +6,9 @@ btn.addEventListener('click', () => {
     if(burger_menu.classList.contains('nav-burger-menu-open')){
         burger_menu.classList.remove('nav-burger-menu-open')
         burger_menu.classList.add('nav-burger-menu-close')
+
     }else{
+
         burger_menu.classList.add('nav-burger-menu-open')
         burger_menu.classList.remove('nav-burger-menu-close')
     }
